a999b6ff4ff3675f1d547ea9a198c79a77f9b323
seojihye <ssb33817@naver.com>
2021-09-13 20:21:43 +0900

    카카오테크 화면 만들기

---
+++ js/common.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.imgWrap').slick(); 
+
+    $('.postSlide').slick({
+        auto: false,
+        infinite: false,
+        slidesToShow: 3,
+        slidesToScroll: 3
+    });
+
+});
